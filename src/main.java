f39e9bc1ba7b10e3a1659c3199082a90cfc9da73
Rannar <rannarzirk@gmail.com>
2023-03-17 14:37:00 +0200

    Nupud on paremad

--- src/main.java
+++ src/main.java
@@ -15,61 +15,46 @@ public class main {
 
 	}
 	public static void asetaNupudLauale(Nupp[][] laud) {
-		for (int rida = 0; rida < 8; rida++) {
+		for (int rida = 0; rida < 2; rida++) {
 			for (int veerg = 0; veerg < 8; veerg++) {
 				if (rida == 1) {
 					//Siin number ka etturi lõppu, sest kuidagi on vaja neid eristada.
 					Nupp mustEttur = new Nupp("ettur" + (veerg + 1), "m");
 					laud[rida][veerg] = mustEttur;
-				}
-				if (rida == 6) {
-					//Siin number ka etturi lõppu, sest kuidagi on vaja neid eristada.
 					Nupp valgeEttur = new Nupp("ettur" + (veerg + 1), "v");
-					laud[rida][veerg] = valgeEttur;
+					laud[6][veerg] = valgeEttur;
 				}
-				//Mustade nuppude lauale asetamine (mitte ettur)
+				//mustade nuppude lauale asetamine (mitte ettur)
 				if (rida == 0 && (veerg == 0 || veerg == 7)) {
 					Nupp mustVanker = new Nupp("vanker", "m");
 					laud[rida][veerg] = mustVanker;
+					Nupp valgeVanker = new Nupp("vanker", "v");
+					laud[7][veerg] = valgeVanker;
 				}
 				if (rida == 0 && (veerg == 1 || veerg == 6)) {
 					Nupp mustHobu = new Nupp("hobune", "m");
 					laud[rida][veerg] = mustHobu;
+					Nupp valgeHobu = new Nupp("hobune", "v");
+					laud[7][veerg] = valgeHobu;
 				}
 				if (rida == 0 && (veerg == 2 || veerg == 5)) {
 					Nupp mustOda = new Nupp("oda", "m");
 					laud[rida][veerg] = mustOda;
+					Nupp valgeOda = new Nupp("oda", "v");
+					laud[7][veerg] = valgeOda;
 				}
 				if (rida == 0 && veerg == 3) {
 					Nupp mustLipp = new Nupp("lipp", "m");
 					laud[rida][veerg] = mustLipp;
+					Nupp valgeLipp = new Nupp("lipp", "v");
+					laud[7][veerg] = valgeLipp;
 				}
 				if (rida == 0 && veerg == 4) {
 					Nupp mustKuningas = new Nupp("kuningas", "m");
 					laud[rida][veerg] = mustKuningas;
-				}
-				//Valgete nuppude lauale asetamine (mitte ettur)
-				if (rida == 7 && (veerg == 0 || veerg == 7)) {
-					Nupp valgeVanker = new Nupp("vanker", "v");
-					laud[rida][veerg] = valgeVanker;
-				}
-				if (rida == 7 && (veerg == 1 || veerg == 6)) {
-					Nupp valgeHobu = new Nupp("hobune", "v");
-					laud[rida][veerg] = valgeHobu;
-				}
-				if (rida == 7 && (veerg == 2 || veerg == 5)) {
-					Nupp valgeOda = new Nupp("oda", "v");
-					laud[rida][veerg] = valgeOda;
-				}
-				if (rida == 7 && veerg == 3) {
-					Nupp valgeLipp = new Nupp("lipp", "v");
-					laud[rida][veerg] = valgeLipp;
-				}
-				if (rida == 7 && veerg == 4) {
 					Nupp valgeKuningas = new Nupp("kuningas", "v");
-					laud[rida][veerg] = valgeKuningas;
+					laud[7][veerg] = valgeKuningas;
 				}
-
 			}
 		}
 	}
