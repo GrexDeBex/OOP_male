82372eace5ef264274863690b386889c6a8ead9b
Rannar <rannarzirk@gmail.com>
2023-03-18 19:31:16 +0200

    Indeksite panek

--- src/Nupp.java
+++ src/Nupp.java
@@ -7,14 +7,18 @@
 public class Nupp {
 	final private String nimi;			// Nupu nimetus
 	final private String varv;			// Millise mängija nupp
+	private int asukohtx;
+	private int asukohty;
 	private int enPassant;				// Kui ettur liigub kaks ruutu salvetatakse käigu number, et kontrollida enpassanti
 	private boolean kasLiikunud;		// Default false, kui ettur, kuningas või vanker liigub esimest korda, siis muutub true-ks
 
-	public Nupp(String nimi, String varv) {
+	public Nupp(String nimi, String varv, int asukohtx, int asukohty) {
 		this.nimi = nimi;
 		this.varv = varv;
 		this.enPassant = 0;
 		this.kasLiikunud = false;
+		this.asukohtx = asukohtx;
+		this.asukohty = asukohty;
 	}
 
 	public String getNimi() {
--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class main {
 
@@ -15,49 +17,82 @@ public class main {
 
 	}
 	public static void asetaNupudLauale(Nupp[][] laud) {
-		for (int rida = 0; rida < 2; rida++) {
 			for (int veerg = 0; veerg < 8; veerg++) {
-				if (rida == 1) {
-					//Siin number ka etturi lõppu, sest kuidagi on vaja neid eristada.
-					Nupp mustEttur = new Nupp("ettur" + (veerg + 1), "m");
-					laud[rida][veerg] = mustEttur;
-					Nupp valgeEttur = new Nupp("ettur" + (veerg + 1), "v");
+				//Siin number ka etturi lõppu, sest kuidagi on vaja neid eristada.
+					Nupp mustEttur = new Nupp("ettur" + (veerg + 1), "m", 1, veerg);
+					laud[1][veerg] = mustEttur;
+					Nupp valgeEttur = new Nupp("ettur" + (veerg + 1), "v", 6, veerg);
 					laud[6][veerg] = valgeEttur;
-				}
-				//mustade nuppude lauale asetamine (mitte ettur)
-				if (rida == 0 && (veerg == 0 || veerg == 7)) {
-					Nupp mustVanker = new Nupp("vanker", "m");
-					laud[rida][veerg] = mustVanker;
-					Nupp valgeVanker = new Nupp("vanker", "v");
+				//Ülejäänud nupud
+				if (veerg == 0) {
+					Nupp mustVanker = new Nupp("vanker1", "m", 0, veerg);
+					laud[0][veerg] = mustVanker;
+					Nupp valgeVanker = new Nupp("vanker1", "v", 7, veerg);
 					laud[7][veerg] = valgeVanker;
 				}
-				if (rida == 0 && (veerg == 1 || veerg == 6)) {
-					Nupp mustHobu = new Nupp("hobune", "m");
-					laud[rida][veerg] = mustHobu;
-					Nupp valgeHobu = new Nupp("hobune", "v");
+				if (veerg == 7) {
+					Nupp mustVanker = new Nupp("vanker2", "m", 0, veerg);
+					laud[0][veerg] = mustVanker;
+					Nupp valgeVanker = new Nupp("vanker2", "v", 7, veerg);
+					laud[7][veerg] = valgeVanker;
+				}
+				if (veerg == 1) {
+					Nupp mustHobu = new Nupp("ratsu1", "m", 0, veerg);
+					laud[0][veerg] = mustHobu;
+					Nupp valgeHobu = new Nupp("ratsu1", "v", 7, veerg);
 					laud[7][veerg] = valgeHobu;
 				}
-				if (rida == 0 && (veerg == 2 || veerg == 5)) {
-					Nupp mustOda = new Nupp("oda", "m");
-					laud[rida][veerg] = mustOda;
-					Nupp valgeOda = new Nupp("oda", "v");
+				if (veerg == 6) {
+					Nupp mustHobu = new Nupp("ratsu2", "m", 0, veerg);
+					laud[0][veerg] = mustHobu;
+					Nupp valgeHobu = new Nupp("ratsu2", "v", 7, veerg);
+					laud[7][veerg] = valgeHobu;
+				}
+				if (veerg == 2) {
+					Nupp mustOda = new Nupp("oda1", "m", 0, veerg);
+					laud[0][veerg] = mustOda;
+					Nupp valgeOda = new Nupp("oda1", "v", 7, veerg);
 					laud[7][veerg] = valgeOda;
 				}
-				if (rida == 0 && veerg == 3) {
-					Nupp mustLipp = new Nupp("lipp", "m");
-					laud[rida][veerg] = mustLipp;
-					Nupp valgeLipp = new Nupp("lipp", "v");
+				if (veerg == 3) {
+					Nupp mustLipp = new Nupp("lipp", "m", 0, veerg);
+					laud[0][veerg] = mustLipp;
+					Nupp valgeLipp = new Nupp("lipp", "v", 7, veerg);
 					laud[7][veerg] = valgeLipp;
 				}
-				if (rida == 0 && veerg == 4) {
-					Nupp mustKuningas = new Nupp("kuningas", "m");
-					laud[rida][veerg] = mustKuningas;
-					Nupp valgeKuningas = new Nupp("kuningas", "v");
+				if (veerg == 4) {
+					Nupp mustKuningas = new Nupp("kuningas", "m", 0, veerg);
+					laud[0][veerg] = mustKuningas;
+					Nupp valgeKuningas = new Nupp("kuningas", "v", 7, veerg);
 					laud[7][veerg] = valgeKuningas;
 				}
-			}
+				if (veerg == 5) {
+					Nupp mustOda = new Nupp("oda2", "m", 0, veerg);
+					laud[0][veerg] = mustOda;
+					Nupp valgeOda = new Nupp("oda2", "v", 7, veerg);
+					laud[7][veerg] = valgeOda;
+				}
 		}
 	}
+	
+	public static void väljastaLaud(Nupp[][] laud){
+		System.out.println("|" + "-----".repeat(8) + "-------" + "|");
+		for (int i = 0; i < 8; i++) {
+			System.out.print("| ");
+			for (int j = 0; j < 8; j++) {
+				if (laud[i][j] == null)
+					System.out.print("    | ");
+				else {
+					String nimi = laud[i][j].getNimi();
+					String värv = laud[i][j].getVarv();
+					System.out.print(nimi.substring(0,1).toUpperCase() + nimi.substring(nimi.length() - 1).toUpperCase() + värv + " | ");
+				}
+			}
+			System.out.print("\n");
+			System.out.println("|" + "-----".repeat(8) +  "-------" + "|");
+		}
+	}
+
 	public static void main(String[] args) {
 		Mangulaud mängulaud = new Mangulaud();
 		asetaNupudLauale(mängulaud.laud);
@@ -66,12 +101,20 @@ public class main {
 				Juhised mängimiseks:\s
 				Liikumiseks tuleb kirjutada konsooli nupu nimetus ning tühikuga eraldada nimest käigu koht.\s
 				Tähised "m" ja "v" tähistavad värvi, kus m on must ning v on valge.\s
-				Käigu näited: (oda b6), (ettur c3), (kuningas a8)\s
+				Käigu näited: (oda1 b6), (ettur5 c3), (kuningas a8)\s
 				Kui ettur jõuab mängulaua lõppu, siis tuleb väikeste tähtedega kirjutada mis nuppu soovitakse.""");
 
 		Mangija mängija1 = new Mangija();
 		Mangija mängija2 = new Mangija();
 		seaMängijateVärvid(mängija1, mängija2);
 		System.out.println("\nEsimene mängija mängib värviga " + mängija1.getVarv() + " ning teine mängija mängib värviga " + mängija2.getVarv() + ".");
+		boolean protsess = true;
+		while (protsess) {
+			väljastaLaud(mängulaud.laud);
+			Scanner sc = new Scanner(System.in);
+			String sisend = sc.nextLine();
+			if (Objects.equals(sisend, "Lõpeta"))
+				protsess = false;
+		}
 	}
 }
